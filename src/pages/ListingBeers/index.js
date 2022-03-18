7894b25fe9da2b95fb212676722a645b95c30981
Rafael Martins <rafael.martins92@outlook.com>
2022-03-18 19:35:07 -0300

    feat: create add beer function

--- src/pages/ListingBeers/index.js
+++ src/pages/ListingBeers/index.js
@@ -8,10 +8,17 @@ import {
   BeerImage,
   BeerName,
   BeerDescription,
+  FormContainer,
 } from "./styles";
 
 function ListingBeers() {
   const [beersList, setBeersList] = useState();
+
+  const [inputVal, setInputValue] = useState({
+    image_url: "",
+    name: "",
+    description: "",
+  });
 
   useEffect(() => {
     loadPage();
@@ -21,6 +28,24 @@ function ListingBeers() {
     const response = await GetBeers();
     setBeersList(response);
   };
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setInputValue({ ...inputVal, [name]: value });
+  }
+
+  const addNewBeer = (event) => {
+    event.preventDefault();
+    setBeersList([
+      ...beersList,
+      {
+        image_url: inputVal.image_url,
+        name: inputVal.name,
+        description: inputVal.description,
+      },
+    ]);
+  };
+
   return (
     <>
       {console.log("beersList", beersList)}
@@ -34,6 +59,55 @@ function ListingBeers() {
             </BeerContainer>
           ))}
       </Container>
+      <FormContainer>
+        <form className="form py-6" onSubmit={addNewBeer}>
+          <h2>Conhece alguma cerveja que deveria estar nesta lista?</h2>
+          <p className="py-3">
+            Ajude os outros cervejeiros e cadastre essa delicia na nossa lista
+          </p>
+          <div className="fields">
+            <div className="input-box">
+              <input
+                type="text"
+                className="image_url"
+                name="image_url"
+                placeholder="Url da imagem"
+                defaultValue={inputVal.image_url}
+                onChange={handleChange}
+                required
+              />
+              <label htmlFor="image_url">Url da imagem</label>
+            </div>
+            <div className="input-box">
+              <input
+                type="text"
+                className="name"
+                name="name"
+                placeholder="Nome"
+                defaultValue={inputVal.name}
+                onChange={handleChange}
+                required
+              />
+              <label htmlFor="name">Nome</label>
+            </div>
+            <div className="input-box">
+              <input
+                type="text"
+                className="description"
+                name="description"
+                placeholder="Descrição"
+                defaultValue={inputVal.description}
+                onChange={handleChange}
+                required
+              />
+              <label htmlFor="description">Descrição</label>
+            </div>
+            <button type="submit" className="button">
+              Enviar
+            </button>
+          </div>
+        </form>
+      </FormContainer>
     </>
   );
 }
